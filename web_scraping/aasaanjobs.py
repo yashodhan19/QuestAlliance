b4f7c1ea351ebb3f7924f5108427adbeabebcacd
kaizer1v <kaizer1v@gmail.com>
2020-03-13 15:28:56 +0530

    merged

--- web_scraping/aasaanjobs.py
+++ web_scraping/aasaanjobs.py
@@ -59,65 +59,6 @@ def get_job_details(job_url):
 
     print(job_details)
 
-<<<<<<< HEAD
-    '''
-    Below is the hierarchy / structure (this structure is below the "Apply CV" button)
-
-    - #job-details
-        - .row
-            - .col-xs-12
-                - .col-xs-12
-                    - .row
-                        - 
-
-    '''
-    sections = job_page.find('div', attrs={'id': 'job-details'})
-
-    for s in sections:
-        divs = s.find_all('div')
-        print(divs.find('div', attrs={'class': 'row'}))
-        
-        import pdb
-        pdb.set_trace()
-
-        # divs[0] = salary, experience, location etc.
-
-        
-        # Additional Details
-        '''
-        divs[1]
-            -> div.col-xs-12
-                -> div.col-xs-12/col.md-12...
-                    -> div.row
-                        -> 2nd div (value)
-                            -> p.text()
-        '''
-
-
-        
-        # Job Requirements
-        '''
-        divs[2]
-            -> div.col-xs-12
-                -> div.col-xs-12/col.md-12...
-                    -> div.row
-                        -> 2nd div (under the title div)
-                            -> loop through every div.row
-                                -> 2nd div (value)
-                                    -> directly find `<p>`.text()
-        '''
-
-
-        # Job Description
-        '''
-        divs[3]
-            -> div.col-xs-12
-                -> div.col-xs-12/col.md-12...
-                    -> div.row
-                        -> 2nd div (under the title div)
-                            -> directly find `<p>`.text()
-        '''
-=======
     sections = soup.find('div', attrs={'id': 'job-details'})
     print(sections)
 
@@ -145,44 +86,6 @@ def process_job_url(jobs, job_category, number_of_pages):
                     get_job_details(job_url)
 
 
-
-
-
-
-
-# jobs =[]
-# job_details= dict()
-# # job container
-# for j in job_urls:
-#     job_url = j['data-job-url']
-#     job_page = kirmi.get_soup(website_baseurl + job_url)
-#
-#     # Salary
-#     job_details['salary_min'] = job_page.find_all('span', attrs={'itemprop': 'minValue'})[0].text
-#     job_details['salary_max'] = job_page.find_all('span', attrs={'itemprop': 'maxValue'})[0].text
-#
-#     # Experience
-#     job_exp = job_page.find('img', attrs={'src': re.compile("https.*icon\-briefcase.*")})
-#     job_details['experience'] = job_exp.parent.parent.parent.find_all('div')[-1].find('p').text
-#
-#     location = job_page.find('img', attrs={'src': re.compile("https.*icon\-pin.*")})
-#     job_details['location'] = location.parent.parent.parent.find_all('div')[1].find('p').text
-#
-#     # ---------------------
-#
-#     # Additional Details
-#
-#     sections = job_page.find('div', attrs={'id': 'job-details'})
-#
-#     for s in sections:
-#         col = s.find_all('div')[0]
-#         print(col.find('div', attrs={'class': 'row'}))
-#
-#     print(sections)
-#     import pdb
-#     pdb.set_trace()
-#
-#
 def run_process():
     job_categories = get_job_categories(
         xml_path='/Users/yashodhanjoglekar/QuestAlliance/web_scraping/job_categories.xml')
@@ -197,42 +100,5 @@ def run_process():
         process_job_url(jobs, job_category, number_of_pages)
 
 
-
-
-            # job_page = scraper.get_soup(website_baseurl + job_url)
-
-
-
-            # # Salary
-            # job_details['salary_min'] = job_page.find_all('span', attrs={'itemprop': 'minValue'})[0].text
-            # job_details['salary_max'] = job_page.find_all('span', attrs={'itemprop': 'maxValue'})[0].text
-            #
-            # # Experience
-            # job_exp = job_page.find('img', attrs={'src': re.compile("https.*icon\-briefcase.*")})
-            # job_details['experience'] = job_exp.parent.parent.parent.find_all('div')[-1].find('p').text
-            #
-            # location = job_page.find('img', attrs={'src': re.compile("https.*icon\-pin.*")})
-            # job_details['location'] = location.parent.parent.parent.find_all('div')[1].find('p').text
-            #
-            # # ---------------------
-            #
-            # # Additional Details
-            #
-            # sections = job_page.find('div', attrs={'id': 'job-details'})
-            #
-            # for s in sections:
-            #     col = s.find_all('div')[0]
-            #     print(col.find('div', attrs={'class': 'row'}))
-            #
-            # print(sections)
-
-#
-#
-#
-#
 if __name__ == "__main__":
-
     run_process()
-
->>>>>>> 0ae76dd225c208788854399c04273d352ac0e28a
-
